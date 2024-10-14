1c8567cd429d56af3cf1bc5bde09b806a2c7d9d8
anurag542 <mauryaanurag0123@gmail.com>
2024-10-14 17:31:33 +0530

    conditions.js

---
+++ javascript/conditions.js
@@ -0,0 +1,48 @@
+num = 234;
+
+if (num % 2 == 0) {
+    console.log('even');
+
+} else {
+    console.log('odd');
+
+}
+
+// WAP to check if person is eligible for DL
+
+age = 18
+if (age >= 18) {
+
+    abc = 'something';
+    let a = 'local';
+    console.log(a);
+    const pi = 3.14;
+    pi = 3.15;
+
+
+
+    console.log('not elgible');
+
+
+} else {
+    console.log('elegible');
+
+}
+
+console.log(abc);
+console.log(a); 
+
+// WAP to check if a number positive,negative or zero
+
+ 
+ if (num >0) {
+     console.log('positive');
+
+    
+ }else if{
+    console.log('negative');
+    
+ } 
+ console.log();
+ 
+
---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+console.log('Hello JavaScript'); 
+
+a= 234;
+
+console.log(a);
+
+myname = 'Anurag Maurya';
+
+console.log(myname);
+
+console.log( typeof myname);
+console.log( typeof a);
+
+price = 34.23;
+
+console.log(typeof price);
+
+console.log( 1+1);
+console.log( '1');
+
+
